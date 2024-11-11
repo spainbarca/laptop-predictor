a7f78d9eb2f7d090c6f54116e3701cb4da68ba66
Noah <practicasjaguar@gmail.com>
2024-11-11 01:19:08 -0500

    traduccion

--- app.py
+++ app.py
@@ -12,20 +12,20 @@ data = pd.read_csv("traineddata.csv")
 st.title("Predictor de Precios de Laptops")
 
 # Inputs de usuario
-company = st.selectbox('Brand', data['Company'].unique())
+company = st.selectbox('Marca', data['Company'].unique())
 type = st.selectbox('Tipo', data['TypeName'].unique())
 ram = st.selectbox('Memoria RAM(en GB)', [2, 4, 6, 8, 12, 16, 24, 32, 64])
 os = st.selectbox('Sistema Operativo', data['OpSys'].unique())
-weight = st.number_input('Peso de la Laptop')
+weight = st.number_input('Peso de la Laptop (en Kg.)')
 touchscreen = st.selectbox('¿Es pantalla tactil?', ['No', 'Si'])
-ips = st.selectbox('¿Es pantalla plana?', ['No', 'Si'])
-screen_size = st.number_input('Screen Size')
+ips = st.selectbox('¿Es LCD?', ['No', 'Si'])
+screen_size = st.number_input('Pulgadas de la pantalla')
 resolution = st.selectbox('Resolución de Pantalla', [
                           '1920x1080', '1366x768', '1600x900', '3840x2160', '3200x1800', '2880x1800', '2560x1600', '2560x1440', '2304x1440'])
 cpu = st.selectbox('CPU', data['CPU_name'].unique())
 hdd = st.selectbox('HDD(en GB)', [0, 128, 256, 512, 1024, 2048])
 ssd = st.selectbox('SSD(en GB)', [0, 8, 128, 256, 512, 1024])
-gpu = st.selectbox('GPU(en GB)', data['Gpu brand'].unique())
+gpu = st.selectbox('GPU (Tarjeta Grafica)', data['Gpu brand'].unique())
 
 # Predicción
 if st.button('Predice el Precio'):
